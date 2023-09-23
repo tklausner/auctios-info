c9dba1a5e60a57384e67487a54d69ee96eb69f55
tklausner <klausnerteddy@gmail.com>
2023-09-23 17:18:28 +0900

    Hero section upgraded

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,49 +8,57 @@ function HeroSection() {
   return (
     <section
       className="hero-section py-5"
-      style={{ backgroundColor: "#FAFAFA" }}
+      style={{ backgroundColor: "#FAFAFA", position: "relative" }}
     >
-      <div className="container">
-        <animated.div style={fadeIn}>
-          <div
+      <animated.div style={fadeIn} className="container">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "2em 0",
+          }}
+        >
+          <img src={logo} width={75} alt="Auctios Logo" />
+          <h1
             style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-              padding: "2em 0",
+              fontSize: "5em",
+              fontFamily: "'Gill Sans', sans-serif", // Ensure fallback
+              marginLeft: "-5px", // pulling the text slightly to overlap with the logo
+              color: "#000",
+              fontWeight: 700,
             }}
           >
-            <img src={logo} width={75} alt={"Auctios Logo"} />
-            <h1
-              style={{
-                fontSize: "5em",
-                fontFamily: "Gill Sans",
-                marginLeft: -5,
-                color: "#000",
-                fontWeight: 700,
-              }}
+            uctios
+          </h1>
+        </div>
+        <p
+          style={{
+            textAlign: "center",
+            color: "#444",
+            fontSize: "1.2em",
+            margin: "1em 0",
+          }}
+        >
+          Experience the future of art auctions. Where beauty meets seamless
+          bidding.
+        </p>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <a
+            href="mailto:info@auctios.xyz?subject=Request%20Demo&body=Hello,%0A%0AI'm interested in requesting a demo. Please get back to me with details."
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button
+              className="btn btn-large custom-button"
+              style={{ fontSize: "1.1em", padding: "0.7em 2em" }}
             >
-              uctios
-            </h1>
-          </div>
-          <p style={{ textAlign: "center", color: "#444" }}>
-            Experience the future of art auctions. Where beauty meets seamless
-            bidding.
-          </p>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <a
-              href="mailto:info@auctios.xyz?subject=Request%20Demo&body=Hello,%0A%0AI'm interested in requesting a demo. Please get back to me with details."
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className="btn btn-large custom-button">
-                Request Demo
-              </button>
-            </a>
-          </div>
-        </animated.div>
-      </div>
+              Request Demo
+            </button>
+          </a>
+        </div>
+      </animated.div>
     </section>
   );
 }
